6138fdee07ef1d01def24d59f654bb3a11925cbb
eduzimmartins22 <dudukofduduzinho@gmail.com>
2024-08-22 22:42:01 -0300

    test

--- src/function.py
+++ src/function.py
@@ -13,4 +13,5 @@ def avaliar_clientes_csv(caminho_arquivo, pontuacao_minima, receita_minima):
         status = "Adequado" if row['adequado'] else "Não Adequado"
         print(f"Cliente: {row['nome']} - Status: {status}")
 
-avaliar_clientes_csv('./files/clientes.csv', pontuacao_minima=70, receita_minima=50000)
+    avaliar_clientes_csv('./files/clientes.csv', pontuacao_minima=70, receita_minima=50000)
+    

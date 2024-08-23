dd7a6e8014a814776acffcc3776a476c64a58b6c
BrenoTonini <brenotoninicosta4@hotmail.com>
2024-08-23 00:27:45 -0300

    feature de validação modularizada

--- src/function.py
+++ src/function.py
@@ -1,12 +1,14 @@
 import pandas as pd
+from utils import validar_csv
 
 def avaliar_clientes_csv(caminho_arquivo, pontuacao_minima, receita_minima):
 
-    df = pd.read_csv(caminho_arquivo)
-    
-    if not all(col in df.columns for col in ['nome', 'pontuacao_engajamento', 'receita_anual']):
-        raise ValueError("O arquivo CSV deve conter as colunas 'nome', 'pontuacao_engajamento', e 'receita_anual'")
-    
+    df = validar_csv(caminho_arquivo)
+
+    if df is None:
+        print("Não é possível avaliar clientes devido a um arquivo inválido.")
+        return
+
     df['adequado'] = (df['pontuacao_engajamento'] >= pontuacao_minima) & (df['receita_anual'] >= receita_minima)
     
     for index, row in df.iterrows():
